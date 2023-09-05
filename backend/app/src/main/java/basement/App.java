a885a266f3306b2d4ac0f4adac016fe5a480b8f3
ferreirawalter <wferreiraramosjunior@gmail.com>
2023-09-05 00:24:17 -0300

    feat: criando integracao com banco

--- backend/app/src/main/java/basement/App.java
+++ backend/app/src/main/java/basement/App.java
@@ -1,8 +1,11 @@
 package basement;
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,29 +20,60 @@ public class App {
         int apiPort = Integer.parseInt(apiPortStr);
         HttpServer server = HttpServer.create(new InetSocketAddress(apiPort), 0);
 
-        server.createContext("/post", new CreatePostSummary());
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Server started on port: " + apiPort);
-
         CassandraConnector client = new CassandraConnector();
         client.connect();
         client.createKeyspace("my_keyspace", "SimpleStrategy", 1);
         client.createTable("posts");
+
+        server.createContext("/post", new CreatePostSummary(client));
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port: " + apiPort);
 
         CqlSession session = client.getSession();
         System.out.println("session " + session);
     }
 
     static class CreatePostSummary implements HttpHandler {
+
+        CassandraConnector client;
+
+        public CreatePostSummary(CassandraConnector client) {
+            this.client = client;
+        }
+
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             if ("POST".equals(exchange.getRequestMethod())) {
                 String requestBody = new String(exchange.getRequestBody().readAllBytes());
                 String response = "Post created: " + requestBody;
 
-                exchange.sendResponseHeaders(200, response.length());
-                exchange.getResponseBody().write(response.getBytes());
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(requestBody);
+
+                    String title = jsonObject.getString("title");
+                    String summary = jsonObject.getString("summary");
+
+                    client.insertPost(title, summary);
+                    exchange.sendResponseHeaders(201, response.length());
+                    exchange.getResponseBody().write(response.getBytes());
+                } catch (JSONException e) {
+                    exchange.sendResponseHeaders(500, response.length());
+                    throw new RuntimeException(e);
+                }
+
+            } else {
+                exchange.sendResponseHeaders(405, 0);
+            }
+        }
+    }
+
+    static class GetSummary implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                exchange.sendResponseHeaders(200, 0);
             } else {
                 exchange.sendResponseHeaders(405, 0);
             }
--- backend/app/src/main/java/basement/UUIDGenerator.java
+++ backend/app/src/main/java/basement/UUIDGenerator.java
@@ -6,4 +6,8 @@ import java.util.Random;
 public class UUIDGenerator {
     private static final Random random = new SecureRandom();
 
+    public static String generateUUID() {
+
+        return null;
+    }
 }
